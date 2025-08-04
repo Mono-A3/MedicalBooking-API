2d0f4794e3401bbfa0d69dc280ff6ee8ec03579a
Andres Araque <aaraqueamaya397@gmail.com>
2025-08-04 16:32:54 -0500

    [UPDATE] Utilizando las vistas genericas de DRF

--- patients/views.py
+++ patients/views.py
@@ -1,14 +1,10 @@
 from .serializers import PatientSerializer
 from .models import Patient
 
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView,
+    RetrieveUpdateDestroyAPIView,
 )
 
 
@@ -18,34 +14,8 @@ class ListPatientView(ListAPIView, CreateAPIView):
     queryset = Patient.objects.all()
 
 
-class DetailPatientsView(APIView):
+class DetailPatientsView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ["GET", "PUT", "DELETE"]
-
-    def get(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = PatientSerializer(patient, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
+    lookup_field = "pk"

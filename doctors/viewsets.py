47ae8588d8b99435ac7a636bf65afb595deee2e3
Andres Araque <aaraqueamaya397@gmail.com>
2025-08-05 17:27:38 -0500

    [ADD] Autenticación y Autorización en APIs

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import DoctorSerializer
 from .models import Doctor
+from .permissions import IsDoctor
 
 
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
     queryset = Doctor.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly, IsDoctor]
 
     @action(["POST"], detail=True, url_path="set-on-vacation")
     def set_on_vacation(self, requests, pk):
